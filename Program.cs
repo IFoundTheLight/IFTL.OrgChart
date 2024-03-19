1798ce8b7f63fbaf31ddae26553273adf8bf274d
Robert Fern II <IFoundTheLight@gmail.com>
2024-03-19 02:40:28 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using IFTL.OrgChart.Lib;
+using Microsoft.VisualBasic.FileIO;
+using System.Data;
+using System.Net;
+using System.Text;
+
+namespace IFTL.OrgChart
+{
+    internal class Program
+    {
+        private static OrganizationChart oc = new OrganizationChart();
+        static void Main(string[] args)
+        {
+            oc.GenerateImage();
+        }
+    }
+}
